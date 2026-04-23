1e3f51b02737f865315db16363549a94d94a9f5d
Rafael Ferreira Lucietto <124806105+Rafa3lFerreira@users.noreply.github.com>
2026-04-23 08:47:19 -0300

    Fix formatting and clean up fila.h

--- fila.h
+++ fila.h
@@ -1,16 +1,12 @@
 #ifndef FILA
 #define FILA
-
 #include "meustiposonlinegdb.h"
-
 typedef void* ElementoDeFila;
-
 typedef struct {
-    ElementoDeFila* vetor; // Elemento vetor [];
+    ElementoDeFila* vetor;
     unsigned int capacidade_inicial, capacidade, qtd_atual,
                  inicio, final;
 } Fila;
-
 boolean nova_fila (Fila* p, unsigned int capacidade);
 boolean guarde_na_fila (Fila* p, ElementoDeFila e);
 boolean recupere_da_fila (Fila p, ElementoDeFila* e);
@@ -18,5 +14,4 @@ boolean remova_elemento_da_fila (Fila* p);
 boolean fila_cheia (Fila p);
 boolean fila_vazia (Fila p);
 boolean free_fila (Fila* p);
-
-#endif // FILA
+#endif
