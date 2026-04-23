33ef10abd2b4d0b96cb8e72030a934b13ac7ad9a
Rafael Ferreira Lucietto <124806105+Rafa3lFerreira@users.noreply.github.com>
2026-04-23 08:47:31 -0300

    Update meustiposonlinegdb.h

--- meustiposonlinegdb.h
+++ meustiposonlinegdb.h
@@ -1,6 +1,5 @@
 #ifndef MEUSTIPOS
 #define MEUSTIPOS
-
 // tipos inteiros
 typedef char I8; // de -128 a 127
 typedef unsigned char U8; // de 0 a 255
@@ -10,16 +9,13 @@ typedef int I32; // de -2147483648 a 2147483647
 typedef unsigned int U32; // de 0 a 4294967295
 typedef long int I64;
 typedef unsigned long int U64;
-
 // tipos reais
 typedef float R32;
 typedef double R64;
 typedef long double R128;
-
 // tipo boolean
 typedef enum {
     false,
     true
 } boolean;
-
 #endif
